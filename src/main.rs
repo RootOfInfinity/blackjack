1141900f004fd9991091a1ac28d40c0fd795252d
William Campbell <numbersdev@protonmail.com>
2024-09-23 19:27:12 -0500

    created and impl card; made a deck of cards

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+enum Suit {
+    Clubs,
+    Hearts,
+    Spades,
+    Diamonds,
+}
+
+struct Card {
+    suit: Suit,
+    num: i32,
+    value: i32,
+}
+impl Card {
+    fn new(suit: Suit, num: i32) -> Self {
+        Card {
+            suit,
+            num,
+            value: num,
+        }
+    }
+    fn to_string(&self) -> String {
+        let suit_string = match self.suit {
+            Suit::Clubs => "Clubs",
+            Suit::Hearts => "Hearts",
+            Suit::Spades => "Spades",
+            Suit::Diamonds => "Diamonds",
+        };
+        let num_string = match self.num {
+            1 => "Ace".to_string(),
+            11 => "Jack".to_string(),
+            12 => "Queen".to_string(),
+            13 => "King".to_string(),
+            number => number.to_string(),
+        };
+        format!("{} of {}", num_string, suit_string)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut deck: Vec<Card> = Vec::new();
+    for i in 0..4 {
+        for j in 1..=13 {
+            let current_suit = match i {
+                0 => Suit::Clubs,
+                1 => Suit::Hearts,
+                2 => Suit::Spades,
+                3 => Suit::Diamonds,
+                _ => panic!(),
+            };
+            deck.push(Card::new(current_suit, j));
+        }
+    }
 }
